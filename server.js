2049d1a0e3a03e1dfc60bd5e3ab4d82427bf9509
mayank <mayankmoolchandani@gmail.com>
2019-08-04 14:19:44 +0530

    [code base pushed]

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser');
+  port = process.env.PORT || 3000;
+
+
+const mysql = require('mysql');
+// connection configurations
+const mc = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'testDb'
+});
+ 
+mc.connect(function(err) {
+    console.log('-----------Mayank--',err,'--------------nnnn');
+    if (err) throw err;
+});
+
+// mc.connect();
+
+app.listen(port);
+
+console.log('API server started on: ' + port);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./app/routes/approutes.js'); //importing route
+routes(app); //register the route
